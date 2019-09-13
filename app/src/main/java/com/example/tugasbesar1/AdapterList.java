9b94b933390dd13fae9b341a8375673481d136cb
dian26swas <dian.swastiani@gmail.com>
2019-09-13 07:25:44 +0700

    mewarna warnain

--- app/src/main/java/com/example/tugasbesar1/AdapterList.java
+++ app/src/main/java/com/example/tugasbesar1/AdapterList.java
@@ -15,14 +15,18 @@ public class AdapterList extends BaseAdapter {
     public ArrayList<Angka> arrayList;
     private Activity activity;
     protected Presenter fl;
+    private int res;
 
     public AdapterList(Activity activity,Presenter fl){
         this.activity = activity;
         this.arrayList = new ArrayList<>();
         this.fl=fl;
+        this.res=0;
     }
 
     public void add(Angka a){
+//        res+=Integer.parseInt(a.getAngka());
+//        Angka newA= new Angka(a.getTanda(),);
         this.arrayList.add(a);
         this.notifyDataSetChanged();
     }
